ac3eb123a6312489af9eb001ab9ab89462cc371e
Ed_Chekanov <kide.da@gmail.com>
2026-01-09 18:28:53 +0300

    fix: for netlify depl

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/APP-YouTube-SPA/',
+  // base: '/APP-YouTube-SPA/',
   plugins: [react()],
 });
